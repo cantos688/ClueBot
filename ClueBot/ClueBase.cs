77473ae96e04bb378733344019370902327d7588
Christopher Antos <cantos1518.8@gmail.com>
2018-11-25 20:48:24 -0500

    Initial commit

---
+++ ClueBot/ClueBase.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClueBot
+{
+    public class ClueBase
+    {
+        public InferenceEngine ie;
+        public VariableArray2D<Double> cptCards;
+        public VariableArray2D<Bernoulli> cardMatrix;
+        public VariableArray2D<Boolean> cardEstimate;
+        public Variable<int> numberOfPlayers;
+        public Range playersRange;
+        public Range numberOfCards;
+
+        public Variable<int> numberOfPeople;
+        public Variable<int> numberOfWeapons;
+        public Variable<int> numberOfRooms;
+
+        public VariableArray<int> playerHandsize;
+
+
+        // needs to be redone with dirichlet random variable 
+
+        public void SetClueSettings(int NumOfPlayers, int NumOfPeople, int NumOfWeapons, int NumOfRooms, int[] PlayersHandSize)
+        {
+            numberOfPlayers = NumOfPlayers;
+            playersRange = new Range(numberOfPlayers+1);
+            numberOfPeople = NumOfPeople;
+            numberOfWeapons = NumOfWeapons;
+            numberOfRooms = NumOfRooms;
+            numberOfCards = new Range(numberOfPeople + numberOfWeapons + numberOfRooms);
+
+            playerHandsize = Variable.Array<int>(playersRange);
+            for(int i =0; i< PlayersHandSize.Length; i++)
+            {
+                playerHandsize[i] = PlayersHandSize[i];
+            }
+            cptCards = Variable.Array<double>(numberOfCards, playersRange);
+            cardMatrix = Variable.Array<Bernoulli>(numberOfCards, playersRange);
+
+
+        }
+
+
+        //passes the form of vectors 
+
+
+
+        public void CreateModel()
+        {
+
+            if (ie == null)
+            {
+                ie = new InferenceEngine();
+            }
+
+
+
+            
+        }
+
+
+        public void updateIndividCard(Variable<int> cardNumber, Variable<int> playerNumber)
+        {
+            cardEstimate[cardNumber, playerNumber].ObservedValue = true;
+        } 
+
+
+        public void updateHand(int[] cardNumbers)
+        {
+            foreach(int i in cardNumbers)
+            {
+                updateIndividCard(i, 1);
+
+            }
+        }
+
+
+      //  public void generateUniformCardProbabilities(double uniformValue)
+       // {
+         //   using (Variable.ForEach(playersRange))
+           // {
+             //   using (Variable.ForEach(numberOfCards))
+               // {
+                 //   cptCards[numberOfCards, playersRange] = uniformValue;
+               // }
+           // }
+        //}
+
+
+        // needs work
+       // public void generateRandom()
+        //{
+        //    using (Variable.ForEach(numberOfCards))
+          //  {
+            //    using (Variable.ForEach(playersRange))
+              //  {
+               //     cptCards[numberOfCards, playersRange] = Variable.DirichletUniform(playersRange.SizeAsInt)[playersRange];
+               // }
+                     
+           // }
+        //}
+
+
+
+
+    }
+}
---
+++ ClueBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClueBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Creates a conditional probability distribution for each card.
+            
+
+
+
+
+            // Updates probability based on cards in hand
+
+
+
+            // Samples from the conditional probabilties subject to the constraints
+
+
+            // Aggregrates samples for new probabilities 
+
+
+
+            // Updates probability based on guess
+            //1. a Bot guesses and sees a card
+            //   b Bot guesses and doesn't see a card
+            //2. a Another Player guesses and sees a card
+            //   b Another player guesses and doesn't see a card
+
+
+            // Returns the most likely guess
+
+
+
+        }
+    }
+}
